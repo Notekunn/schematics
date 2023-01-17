61cfe54b7fc9b059d2f321df2302a810baa779c8
Neil <cuong.nl5.kma@gmail.com>
2023-01-17 20:39:31 +0700

    fix: update template for command and queries

--- src/schematics/command/files/cqrs/commands/handler/__name@dasherize__.handler.ts
+++ src/schematics/command/files/cqrs/commands/handler/__name@dasherize__.handler.ts
@@ -1,9 +1,10 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { <%= classify(name) %>Command } from '../impl/<%= dasherize(name) %>.command';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+
+import { <%= classify(name) %>Command } from '../impl/<%= dasherize(name) %>.command'
 
 @CommandHandler(<%= classify(name) %>Command)
-export class <%= classify(name) %>CommandHandler implements ICommandHandler<<%= classify(name) %>Command>{
-    async execute(command: <%= classify(name) %>Command) {
-        return command
-    }
+export class <%= classify(name) %>CommandHandler implements ICommandHandler<<%= classify(name) %>Command> {
+  async execute(command: <%= classify(name) %>Command) {
+    return command
+  }
 }
--- src/schematics/query/files/cqrs/queries/handler/__name@dasherize__.handler.ts
+++ src/schematics/query/files/cqrs/queries/handler/__name@dasherize__.handler.ts
@@ -1,9 +1,10 @@
-import { QueryHandler, IQueryHandler } from '@nestjs/cqrs';
-import { <%= classify(name) %>Query } from '../impl/<%= dasherize(name) %>.query';
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
+
+import { <%= classify(name) %>Query } from '../impl/<%= dasherize(name) %>.query'
 
 @QueryHandler(<%= classify(name) %>Query)
-export class <%= classify(name) %>QueryHandler implements IQueryHandler<<%= classify(name) %>Query>{
-    async execute(query: <%= classify(name) %>Query) {
-        return query
-    }
+export class <%= classify(name) %>QueryHandler implements IQueryHandler<<%= classify(name) %>Query> {
+  async execute(query: <%= classify(name) %>Query) {
+    return query
+  }
 }
