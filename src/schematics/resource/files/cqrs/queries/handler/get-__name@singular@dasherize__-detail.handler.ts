c1919d37e84fca92f2c2962686a1b9d5125b7ca1
Trần Đức Cường <36887165+Notekunn@users.noreply.github.com>
2024-01-27 10:42:22 +0700

    fix: controller resource space lint (#7)
    * fix: lint
    
    * fix: lint
    
    * fix: lint for controller

--- src/schematics/resource/files/cqrs/queries/handler/get-__name@singular@dasherize__-detail.handler.ts
+++ src/schematics/resource/files/cqrs/queries/handler/get-__name@singular@dasherize__-detail.handler.ts
@@ -1,6 +1,6 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
 
-import { Get<%= classify(singular(name)) %>DetailQuery } from '../impl/get-<%= dasherize(name) %>-detail.query'
+import { Get<%= classify(singular(name)) %>DetailQuery } from '../impl/get-<%= dasherize(singular(name)) %>-detail.query'
 
 @QueryHandler(Get<%= classify(singular(name)) %>DetailQuery)
 export class Get<%= classify(singular(name)) %>DetailQueryHandler implements IQueryHandler<Get<%= classify(singular(name)) %>DetailQuery> {
--- src/schematics/resource/files/cqrs/queries/handler/get-__name@singular@dasherize__-list.handler.ts
+++ src/schematics/resource/files/cqrs/queries/handler/get-__name@singular@dasherize__-list.handler.ts
@@ -1,6 +1,6 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
 
-import { Get<%= classify(singular(name)) %>ListQuery } from '../impl/get-<%= dasherize(name) %>-list.query'
+import { Get<%= classify(singular(name)) %>ListQuery } from '../impl/get-<%= dasherize(singular(name)) %>-list.query'
 
 @QueryHandler(Get<%= classify(singular(name)) %>ListQuery)
 export class Get<%= classify(singular(name)) %>ListQueryHandler implements IQueryHandler<Get<%= classify(singular(name)) %>ListQuery> {
